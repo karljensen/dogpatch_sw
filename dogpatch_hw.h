f1902e1af17df5744334ad2684e685f8f24cb7a0
Matt Voss <matt@barbarypartners.com>
2024-10-30 17:06:39 -0700

    mvoss -- from 4 to 6 tables

--- dogpatch_hw.h
+++ dogpatch_hw.h
@@ -207,8 +207,8 @@ typedef struct dogpatch_leg_param_t {
     uint32_t risk_price_buy = 0;
     uint8_t  delta : 7 = 0; // NOTE delta 0-127 with 64 equals delta of 1
     uint8_t  slot_en: 1 = 0;
-    uint8_t  source_table: 2 = 0;
-    uint8_t  reserved1: 6 = 0;
+    uint8_t  source_table: 3 = 0;
+    uint8_t  reserved1: 5 = 0;
     uint8_t reserved2 = 0;
 } dogpatch_leg_param_t;
 
