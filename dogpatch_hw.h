8b37e19f6c10675b41fd6450b4471f9ffc194d39
Karl Jensen <Karl@pntfin.com>
2025-05-14 10:42:21 -0600

    Fixing bad extern reference in dogpatch_hw.h

--- dogpatch_hw.h
+++ dogpatch_hw.h
@@ -327,7 +327,7 @@ private:
     void set_tcp_cksm(uint8_t session);
 };
 
-extern const char* RISK_CODES[17];
+extern const char* RISK_CODES[18];
 
 void print_mon( dogpatch_mon_pkt_t * pkt);
 ssize_t kexanic_receive_frame(exanic_rx_t *rx, char *rx_buf, size_t rx_buf_size,
